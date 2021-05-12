bdb86b02bb9a5714df77eff860aa0c1a0102e4bc
Andres Figueroa <andres.figueroa,a@gmail.com>
2021-05-12 19:43:40 -0400

    Created Grade Model

--- REST_API/main.py
+++ REST_API/main.py
@@ -28,6 +28,15 @@ class ClassesModel(db.Model):
 
     def __repr__(self):
         return f"{name}: {description}"
+    
+class GradesModel(db.Model):
+    __tablename__ = 'grades'
+    gradeID = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return f"{name}: {description}"
 
 #db.create_all()
 
